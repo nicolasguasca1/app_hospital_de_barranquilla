8cd0036540aaf469d997ade398e0c865e07af307
Nicolas Guasca <nicolas.guasca@gmail.com>
2021-10-09 01:26:48 +0200

    Formulario de pacientes terminado

--- forms.py
+++ forms.py
@@ -1,31 +1,32 @@
 from flask_wtf import FlaskForm
-from wtforms import TextField, PasswordField
+from wtforms import TextField, PasswordField, StringField
+from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, EqualTo, Length
 
 # Set your classes here.
 
 
 class RegisterFormPac(FlaskForm):
-    name = TextField(
-        'nombre', validators=[DataRequired(), Length(min=6, max=25)]
+    name = StringField(
+        'Nombres', validators=[DataRequired(), Length(min=6, max=25)]
     )
-    lastname = TextField(
-        'apellido', validators=[DataRequired(), Length(min=6, max=25)]
+    lastname = StringField(
+        'Apellidos', validators=[DataRequired(), Length(min=6, max=25)]
     )
-    id = TextField(
-        'número de identificación', validators=[DataRequired(), Length(min=6, max=25)]
+    id = StringField(
+        'Número de identificación', validators=[DataRequired(), Length(min=6, max=25)]
     )
-    birthdate = TextField(
-        'fecha de nacimiento', validators=[DataRequired(), Length(min=6, max=25)]
+    birthdate = DateField(
+        'Fecha de nacimiento', format='%d-%m-%Y' 
     )
-    username = TextField(
-        'nombre de usuario', validators=[DataRequired(), Length(min=6, max=25)]
-    )
-    email = TextField(
+    email = StringField(
         'Correo electrónico', validators=[DataRequired(), Length(min=6, max=40)]
     )
+    username = StringField(
+        'Defina un nombre de usuario', validators=[DataRequired(), Length(min=6, max=25)]
+    )
     password = PasswordField(
-        'Contraseña', validators=[DataRequired(), Length(min=6, max=40)]
+        'Defina una contraseña', validators=[DataRequired(), Length(min=6, max=40)]
     )
     confirm = PasswordField(
         'Repetir contraseña',
